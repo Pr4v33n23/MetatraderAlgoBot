a9dbe9076e745120b92182b3dfe37d7d2b7f742a
Pr4v33n23 <praveen.jaisankar@gmail.com>
2023-10-23 00:39:55 +0100

    added notebook for analysis

--- src/metatrader/__main__.py
+++ src/metatrader/__main__.py
@@ -1,22 +1,24 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import sqlite3
 
 from utils.factory import MetatraderFactory
 
 metatrader = MetatraderFactory.get_metatrader()
+sql_connection = sqlite3.connect('src\\metatrader\\data\\tickers_data.db')
 
 sns.set_theme()
 
 mt5 = metatrader.connect()
 
 symbol = "AAPL"
-bars = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_D1, 0, 100)
+bars = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_D1, 0, 1000)
 
 df = pd.DataFrame(bars)
 df["time"] = pd.to_datetime(df["time"], unit="s")
 sns.lineplot(data=df, x="time", y="close")
 
+df.to_sql('aapl', con=sql_connection, if_exists='replace')
 
-plt.show()
 metatrader.disconnect()
