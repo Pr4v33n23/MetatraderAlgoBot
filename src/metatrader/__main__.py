8afcbe37aa5246c283fd0e0f6353a0be4390c1b9
Pr4v33n23 <praveen.jaisankar@gmail.com>
2023-10-29 13:21:01 +0000

    added csv files for analysis

--- src/metatrader/__main__.py
+++ src/metatrader/__main__.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sqlite3
+from datetime import datetime
+import plotly.express as px
+import plotly.graph_objects as go
+import pytz
 
 from metatrader.utils.factory import MetatraderFactory
 from metatrader.strategies.ema_crossover import EMACrossover
 
+
 metatrader = MetatraderFactory.get_metatrader()
 sql_connection = sqlite3.connect("./data/tickers_data.db")
 
-
 mt5 = metatrader.connect()
-
-ema_crossover = EMACrossover(mt5)
-ema_crossover.run_strategy()
